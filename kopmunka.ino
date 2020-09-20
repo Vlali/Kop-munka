bd52f3b08c852cbfd666b7be95d35aa0258b7887
Vlali <54359026+Vlali@users.noreply.github.com>
2020-09-20 20:46:15 +0200

    Add files via upload

--- kopmunka.ino
+++ kopmunka.ino
@@ -5,7 +5,10 @@
 #define ONE_WIRE_BUS 11
 #define DHT11_PIN 12
 dht11 DHT11;
-LiquidCrystal_I2C lcd(0x27,16,2);
+LiquidCrystal_I2C lcd1(0x27,20,4);
+LiquidCrystal_I2C lcd2(0x23,16,2);
+
+
 
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices  
 // (not just Maxim/Dallas temperature ICs) 
@@ -24,44 +27,59 @@ int mhiba;   // moisture,föld víztartalma
 int vhiba;   // víz hőmérséklete 
 int allapot=0; // Az lcd-én épp melyik értéket jelenítem meg,gombnyomással nő az értéke 2ig,után kinullázódik.Ha hibajelzés van nincs módunk tovább lépni.
 int hang=100;  //Hangszóró értéke
+int kesleltetes=20;
 int case_value;
-
+int a;
 
 void hibak(){
+ 
+ a=case_value;
 if(fhiba==1 && hhiba==0 && phiba==0 && mhiba==0 && vhiba==0){      
-    case_value=0;}
+    case_value=0;
+    }
   else if(fhiba==0 && hhiba==1 && phiba==0 && mhiba==0 && vhiba==0){            
-    case_value=1;}
+    case_value=1;
+     }
   
   else if(fhiba==0 && hhiba==0 && phiba==1 && mhiba==0 && vhiba==0){
-    case_value=2;}
+    case_value=2;
+     }
   
   else if(fhiba==0 && hhiba==0 && phiba==0 && mhiba==1 && vhiba==0){
-    case_value=3;}
+    case_value=3;
+     }
   
   else if(fhiba==0 && hhiba==0 && phiba==0 && mhiba==0 && vhiba==1){
-    case_value=4;}
+    case_value=4;
+    }
   
   else if(fhiba==0 && hhiba==1 && phiba==1 && mhiba==0 && vhiba==0){
-    case_value=5;}
+    case_value=5;
+     }
   
   else if(fhiba==1 && hhiba==0 && phiba==0 && mhiba==1 && vhiba==0){
-    case_value=6;}
+    case_value=6;
+     }
   
   else if(fhiba==0 && hhiba==1 && phiba==0 && mhiba==1 && vhiba==0){
-    case_value=7;}
+    case_value=7;
+     }
   
   else if(fhiba==0 && hhiba==0 && phiba==1 && mhiba==1 && vhiba==0){
-    case_value=8;}
+    case_value=8;
+    }
  
   else if(fhiba==0 && hhiba==0 && phiba==0 && mhiba==1 && vhiba==1){
-    case_value=9;}
+    case_value=9;
+     }
 
   else if(fhiba==1 && hhiba==1 && phiba==0 && mhiba==0 && vhiba==0){
-    case_value=10;}
+    case_value=10;
+     }
   
   else if(fhiba==1 && hhiba==0 && phiba==1 && mhiba==0 && vhiba==0){
-    case_value=11;}
+    case_value=11;
+    }
   
  else if(fhiba==1 && hhiba==0 && phiba==0 && mhiba==0 && vhiba==1){
     case_value=12;}
@@ -77,13 +95,18 @@ if(fhiba==1 && hhiba==0 && phiba==0 && mhiba==0 && vhiba==0){
   
   else{                                          
     case_value=16;}
-}
+  if(case_value!=a){
+    lcd2.clear();}
+  }
+
 
 
 
 void setup(){
-  lcd.init();
-  lcd.backlight();               
+  lcd1.init();
+  lcd1.backlight();
+  lcd2.init();
+  lcd2.backlight();               
   Serial.begin(9600);
   sensors.begin();   
   pinMode(hangszoro,OUTPUT);                //hangszóró
@@ -121,7 +144,7 @@ void loop(){
     phiba=0;}
   else{phiba=1;}
 
-  if (moisture>100){
+  if (moisture>150){
     mhiba=0;}
   else{mhiba=1;}  
  
@@ -133,192 +156,217 @@ void loop(){
   hibak();
   
   switch(case_value){
+   
     case 0:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a feny");
-    lcd.setCursor(0,1);
-    lcd.print(light);
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a feny");
+    lcd2.setCursor(0,1);
+    lcd2.print(light);
     if (hang==0){noTone(hangszoro);}          
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);               
+   
     break;
   
     case 1:            
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a homerseklet");
-    lcd.setCursor(0,1);
-    lcd.print(homerseklet);
+   
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a homerseklet");
+    lcd2.setCursor(0,1);
+    lcd2.print(homerseklet);
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+  
      break; 
   
     case 2:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a paratartalom");
-    lcd.setCursor(0,1);
-    lcd.print(paratartalom);
+   
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a paratartalom");
+    lcd2.setCursor(0,1);
+    lcd2.print(paratartalom);
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+   
      break; 
   
   
     case 3:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Keves a vizt.");
-    lcd.setCursor(0,1);
-    lcd.print(moisture);
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Keves a vizt.");
+    lcd2.setCursor(0,1);
+    lcd2.print(moisture);
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,LOW);
+    
      break; 
   
     case 4:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a vizho");
-    lcd.setCursor(0,1);
-    lcd.print(vizh);
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a vizho");
+    lcd2.setCursor(0,1);
+    lcd2.print(vizh);
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+   
      break; 
   
     case 5:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a homerseklet");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a paratartalom");
+   
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a homerseklet");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a paratartalom");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+     
      break; 
   
     case 6:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a feny");
-    lcd.setCursor(0,1);
-    lcd.print("Keves a vizt.");
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a feny");
+    lcd2.setCursor(0,1);
+    lcd2.print("Keves a vizt.");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+    
      break; 
   
     case 7:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a homerseklet");
-    lcd.setCursor(0,1);
-    lcd.print("Keves a vizt.");
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a homerseklet");
+    lcd2.setCursor(0,1);
+    lcd2.print("Keves a vizt.");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+   
      break; 
   
     case 8:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Keves a vizt.");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a paratartalom:");
+   
+    lcd2.setCursor(0,0);
+    lcd2.print("Keves a vizt.");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a paratartalom:");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+  
      break; 
   
     case 9:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Keves a vizt.");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a vizho");
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Keves a vizt.");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a vizho");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+     
      break; 
 
     case 10:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a feny");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a homerseklet");
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a feny");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a homerseklet");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+   
     break; 
   
     case 11:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a feny");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a paratartalom:");
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a feny");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a paratartalom:");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+   
     break; 
   
     case 12:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a feny");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a vizho:");
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a feny");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a vizho:");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+ 
     break; 
   
     case 13:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a homerseklet");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a vizho");
+   
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a homerseklet");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a vizho");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+   
     break; 
   
     case 14:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Nagy a vizho");
-    lcd.setCursor(0,1);
-    lcd.print("Nagy a paratartalom");
+   
+    lcd2.setCursor(0,0);
+    lcd2.print("Nagy a vizho");
+    lcd2.setCursor(0,1);
+    lcd2.print("Nagy a paratartalom");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,HIGH);
+    
      break; 
   
     case 15:
+   
+    lcd2.setCursor(0,0);
+    lcd2.print("Minden ertek");
+    lcd2.setCursor(0,1);
+    lcd2.print("megfelelo");
     noTone(hangszoro);
     digitalWrite(motor,HIGH);
+     
      break; 
     
     case 16:
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("2-nel tobb ertek");
-    lcd.setCursor(0,1);
-    lcd.print("nem megfelelo!");
+    
+    lcd2.setCursor(0,0);
+    lcd2.print("2-nel tobb ertek");
+    lcd2.setCursor(0,1);
+    lcd2.print("nem megfelelo!");
     if (hang==0){noTone(hangszoro);}
     else{tone(hangszoro,hang);}
     digitalWrite(motor,LOW);
+   
     break; 
+    
   }
 
   
 int  gomb=digitalRead(gombnumber);   //a gombbal variálom az állapotokat.
   if (gomb==0){
+    lcd1.clear();
     if (allapot==0){
       allapot=1;}
     else if (allapot==1){
@@ -330,36 +378,36 @@ int  gomb=digitalRead(gombnumber);   //a gombbal variálom az állapotokat.
   
  
   if (allapot==0 && fhiba==0 && hhiba==0 && phiba==0 && mhiba==0 && vhiba==0){     //csak akkor érvényesül ha nincs szükség a hibajelzésre.
-   lcd.clear();
-   lcd.setCursor(0,0); 
-   lcd.print("Temp:");
-   lcd.setCursor(9,0);
-   lcd.print(homerseklet);
-   lcd.print((char)223);
-   lcd.print("C");
-   lcd.setCursor(0,1);
-   lcd.print("Humidity:");
-   lcd.print(paratartalom);
-   lcd.print("%");
-   delay(20);}
+   
+   lcd1.setCursor(0,0); 
+   lcd1.print("Temp:");
+   lcd1.setCursor(9,0);
+   lcd1.print(homerseklet);
+   lcd1.print((char)223);
+   lcd1.print("C");
+   lcd1.setCursor(0,1);
+   lcd1.print("Humidity:");
+   lcd1.print(paratartalom);
+   lcd1.print("%");
+   delay(kesleltetes);}
   
   if (allapot==1 && fhiba==0 && hhiba==0 && phiba==0 && mhiba==0 && vhiba==0){
-   lcd.clear();
-   lcd.setCursor(0,0);
-   lcd.print("Fenyerosseg:");
-   lcd.setCursor(0,1);
-   lcd.print(light);
-   delay(20);}
+   
+   lcd1.setCursor(0,0);
+   lcd1.print("Fenyerosseg:");
+   lcd1.setCursor(0,1);
+   lcd1.print(light);
+   delay(kesleltetes);}
 
   if (allapot==2 && fhiba==0 && hhiba==0 && phiba==0 && mhiba==0 && vhiba==0){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Viztartalom");
-    lcd.setCursor(12,0);
-    lcd.print(moisture);
-    lcd.setCursor(0,1);
-    lcd.print("Vizho");
-    lcd.setCursor(11,1);
-    lcd.print(vizh);
-    delay(20);}}
+ 
+    lcd1.setCursor(0,0);
+    lcd1.print("Viztartalom");
+    lcd1.setCursor(12,0);
+    lcd1.print(moisture);
+    lcd1.setCursor(0,1);
+    lcd1.print("Vizho");
+    lcd1.setCursor(11,1);
+    lcd1.print(vizh);
+    delay(kesleltetes);}}
  
